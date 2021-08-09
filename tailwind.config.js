eceef03e12126d5535423800d0b55957632c6c94
Felipe Espinoza <felipe.espinoza@tokencash.mx>
2021-08-09 12:25:33 -0500

    configuracion tailwindcss, colores y tamaños de fuente

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: [
     './resources/**/*.blade.php',
@@ -6,10 +8,66 @@ module.exports = {
   ],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        'xxs': '.65rem'
+      },
+      colors: {
+        blue: {
+          lighter: '#EAF4FE',
+          light: '#208AFF',
+          DEFAULT: '#0560FC',
+          dark: '#003CBE',
+          darker: '#3A10E5'
+        },
+        gray: {
+          light: '#F7F9FA',
+          lighter: '#FCFDFE',
+          DEFAULT: '#DBDCE0',
+          darker: '#181818',
+          dark: '#333030',
+        },
+        green: {
+          lightest: '#CCE1D6',
+          lighter: '#2BB970',
+          light: '#2BB970',
+          DEFAULT: '#2BB970',
+          dark: '#67A786',
+          darker: '#1C7B4A'
+        },
+        orange: {
+          lighter: '#FFF0E5',
+          light: '#F77E63',
+          DEFAULT: '#FF6610',
+          dark: '#EF542E'
+        },
+        red: {
+          lighter: '#EBA39F',
+          light: '#D23228',
+          DEFAULT: '#E13516',
+          dark: '#D23228',
+          darker: '#921108'
+        },
+        white: '#FFFFFF',
+        yellow: {
+          light: '#FFD300',
+          DEFAULT: '#F0CC00',
+          dark: '#CCA900'
+        }
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      extend: {
+        fontWeight: ['hover']
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
